661dc79d6e4d2cfb6389cecab7ca6bde6fdee96e
Whasabi <jmartincarratala@outlook.es>
2024-09-18 22:11:38 +0200

    actualización primeros archivos

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio Fotográfico</title>
+
+    <!-- Enlace al favicon -->
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- Enlace a los estilos globales -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <!-- Contenedor principal para React -->
+    <div id="root"></div>
+
+    <!-- Enlace al archivo principal de JavaScript que ejecutará React -->
+    <script src="js/main.js"></script>
+</body>
+</html>
