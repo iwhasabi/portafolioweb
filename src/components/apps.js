7a1d071031de083ecfd87bfc70b961d450d57344
iwhasabi <94785122+iwhasabi@users.noreply.github.com>
2024-09-19 09:20:25 +0200

    He creado apps.js

---
+++ src/components/apps.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Portafolio Fotográfico</h1>
+            <p>¡Bienvenido a mi portafolio!</p>
+        </div>
+    );
+}
+
+export default App;
